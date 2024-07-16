10161b969549567075776cf48e5324c4a0907993
Cromeroo <cromeroo@unal.edu.co>
2024-07-15 22:27:12 -0500

    HomeController

---
+++ src/main/java/com/ejercicio1/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ejercicio1.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Spring Boot application!";
+    }
+}
